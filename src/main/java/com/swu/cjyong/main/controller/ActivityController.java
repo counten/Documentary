9f8019daed5750b0f2517db8752d4614eeb7a29d
cai123nb <2686600303@qq.com>
2017-10-21 14:18:09 +0800

    [ADD] 添加七牛云上传接口

--- src/main/java/com/swu/cjyong/main/controller/ActivityController.java
+++ src/main/java/com/swu/cjyong/main/controller/ActivityController.java
@@ -1,5 +1,13 @@
 package com.swu.cjyong.main.controller;
 
+import com.google.gson.Gson;
+import com.qiniu.common.QiniuException;
+import com.qiniu.common.Zone;
+import com.qiniu.http.Response;
+import com.qiniu.storage.Configuration;
+import com.qiniu.storage.UploadManager;
+import com.qiniu.storage.model.DefaultPutRet;
+import com.qiniu.util.Auth;
 import com.swu.cjyong.main.entity.Activity;
 import com.swu.cjyong.main.entity.User;
 import com.swu.cjyong.main.entity.dto.ActivityIndex;
@@ -149,7 +157,35 @@ public class ActivityController {
     }
 
     private boolean uploadFileToQiniuYun(String fileUrl) {
-        
+        //构造一个带指定Zone对象的配置类
+        Configuration cfg = new Configuration(Zone.zone0());
+        //...其他参数参考类注释
+        UploadManager uploadManager = new UploadManager(cfg);
+        //...生成上传凭证，然后准备上传
+        String accessKey = "your access key";
+        String secretKey = "your secret key";
+        String bucket = "your bucket name";
+        //如果是Windows情况下，格式是 D:\\qiniu\\test.png
+        String localFilePath = "/home/qiniu/test.png";
+        //默认不指定key的情况下，以文件内容的hash值作为文件名
+        String key = null;
+        Auth auth = Auth.create(accessKey, secretKey);
+        String upToken = auth.uploadToken(bucket);
+        try {
+            Response response = uploadManager.put(localFilePath, key, upToken);
+            //解析上传成功的结果
+            DefaultPutRet putRet = new Gson().fromJson(response.bodyString(), DefaultPutRet.class);
+            System.out.println(putRet.key);
+            System.out.println(putRet.hash);
+        } catch (QiniuException ex) {
+            Response r = ex.response;
+            System.err.println(r.toString());
+            try {
+                System.err.println(r.bodyString());
+            } catch (QiniuException ex2) {
+                //ignore
+            }
+        }
         return true;
     }
 

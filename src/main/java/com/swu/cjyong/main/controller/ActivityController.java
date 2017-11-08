030d8cbeffdbf3d7fa8d2f385cc8a800194bc166
MiceWang <miclewang@outlook.com>
2017-11-08 18:45:03 +0800

    [FIX] 添加一些朱思

--- src/main/java/com/swu/cjyong/main/controller/ActivityController.java
+++ src/main/java/com/swu/cjyong/main/controller/ActivityController.java
@@ -1,31 +1,18 @@
 package com.swu.cjyong.main.controller;
 
-import com.google.gson.Gson;
-import com.qiniu.common.QiniuException;
-import com.qiniu.common.Zone;
-import com.qiniu.http.Response;
-import com.qiniu.storage.Configuration;
-import com.qiniu.storage.UploadManager;
-import com.qiniu.storage.model.DefaultPutRet;
-import com.qiniu.util.Auth;
 import com.swu.cjyong.main.entity.Activity;
 import com.swu.cjyong.main.entity.User;
 import com.swu.cjyong.main.entity.dto.ActivityIndex;
-import com.swu.cjyong.main.entity.dto.MemberCount;
 import com.swu.cjyong.main.service.ActivityService;
 import com.swu.cjyong.main.util.FileUploadUtil;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.List;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/activity")
